59e9147c2e67654a516fe14a54db504ce1de2dba
Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
2021-05-18 12:52:02 -0800

    fix: ok

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-yarn run build && yarn unlink && yarn link
+yarn run build 
+
+yarn unlink @sonatype/js-sona-types | true && yarn link @sonatype/js-sona-types
 
 cd examples/node && yarn link @sonatype/js-sona-types && yarn install --force && yarn start
 cd ../react-test-app && yarn link @sonatype/js-sona-types && yarn install --force && yarn build
