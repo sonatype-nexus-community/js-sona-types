db4e2ccb5a3534ecee78b95f90a98170294ce2e5
Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
2021-05-18 12:57:57 -0800

    fix: only link

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 yarn run build 
 
-yarn unlink @sonatype/js-sona-types | true && yarn link @sonatype/js-sona-types
+yarn link
 
 cd examples/node && yarn link @sonatype/js-sona-types && yarn install --force && yarn start
 cd ../react-test-app && yarn link @sonatype/js-sona-types && yarn install --force && yarn build
